98b03362112e1d555aae0d12ec5d446ebf345bea
K_COMPANY_win7 <1358109029@qq.com>
2019-11-18 16:53:15 +0800

    打包练习

---
+++ 小专栏/打包--台风实时路径/app/main.js
@@ -0,0 +1,1 @@
+import mapApp from './app.js';
--- 小专栏/打包--台风实时路径/webpack.config.js
+++ 小专栏/打包--台风实时路径/webpack.config.js
@@ -1,2 +1,17 @@
 const webpack = require('webpack');
 
+module.exports = {
+    mode: "development",
+    entry: __dirname+"/app/main.js",
+    output:{
+        path: __dirname+"/public",
+        filename: "bundle.js"
+    },
+    devtool: "eval-source-map",
+    devServer: {
+
+    },
+    module: {
+        
+    }
+}
